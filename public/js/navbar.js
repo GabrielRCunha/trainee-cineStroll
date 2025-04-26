0c8242c101742c66d8f621095f41304fe8ccbae7
Theo Faria <theocarfar@gmail.com>
2025-04-26 09:30:21 -0300

    Adiciona nome explicativo das variáveis js

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,17 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     const hamburguer = document.querySelector('.hamburguer');
-    const navMobile = document.querySelector('.nav-mobile');
+    const navbar_mobile = document.querySelector('.nav-mobile');
 
     hamburguer.addEventListener('click', function () {
         hamburguer.classList.toggle('active');
-        navMobile.classList.toggle('active');
+        navbar_mobile.classList.toggle('active');
     });
 
-    // Para fechar o menu mobile quando algum ícone for clicado
     document.querySelectorAll('.nav-mobile a').forEach(item => {
         item.addEventListener('click', function () {
             hamburguer.classList.remove('active');
-            navMobile.classList.remove('active');
+            navbar_mobile.classList.remove('active');
         });
     });
 });
