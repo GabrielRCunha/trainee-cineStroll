2f7da7a5151bc1ed7fa9e92a3f266da946be797f
Theo Faria <theocarfar@gmail.com>
2025-04-29 15:15:18 -0300

    Adiciona media queries à tabela

--- public/js/tabelaDePosts.js
+++ public/js/tabelaDePosts.js
@@ -1,29 +1,41 @@
 const pagina = document.querySelectorAll('.pagina')
 const voltar = document.querySelector('.voltar')
 const passar = document.querySelector('.passar')
+const logo = document.querySelector('.logo-barra-superior')
 
 let indexPagAtiva = 0;
 
-function trocarPaginaAtiva(index){
+function trocarPaginaAtiva(index) {
     pagina.forEach(pag => pag.classList.remove('ativa'));
     pagina[index].classList.add('ativa');
     indexPagAtiva = index;
 }
 
+function atualizaLogo() {
+    if (window.innerWidth <= 768) {
+        logo.classList.add('inativo')
+    }
+    else {
+        logo.classList.remove('inativo')
+    }
+}
+
 pagina.forEach((pag, index) => {
-    pag.addEventListener('click', function(event){
+    pag.addEventListener('click', function (event) {
         trocarPaginaAtiva(index);
     })
 })
 
-voltar.addEventListener('click', function(event){
-    if( indexPagAtiva > 0 ){
+voltar.addEventListener('click', function (event) {
+    if (indexPagAtiva > 0) {
         trocarPaginaAtiva(indexPagAtiva - 1)
     }
 })
 
-passar.addEventListener('click', function(event){
-    if( indexPagAtiva < pagina.length - 1 ){
+passar.addEventListener('click', function (event) {
+    if (indexPagAtiva < pagina.length - 1) {
         trocarPaginaAtiva(indexPagAtiva + 1)
     }
 })
+
+window.addEventListener('resize', atualizaLogo)
