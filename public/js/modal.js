c650e3cf04747c76631019c50c506a61b7844e03
RyanPAalvim <ryalvim84@gmail.com>
2025-05-07 16:56:20 -0300

    Adiciona modal de visualizar

---
+++ public/js/modal.js
@@ -0,0 +1,25 @@
+const tela = document.querySelector('.fundoModal');
+
+function abrirModal(idModal){
+    document.getElementById(idModal).style.display = 'flex';
+    tela.style.display = 'block';
+}
+
+function fecharModal(idModal){
+    document.getElementById(idModal).style.display = 'none';
+    tela.style.display = 'none';
+}
+
+function mostrarSenha(idIcone, idInput){
+    const icone = document.getElementById(idIcone);
+    const input = document.getElementById(idInput);
+    
+    if(input.type === 'password'){
+        input.setAttribute('type', 'text');
+        icone.classList.replace('bi-eye', 'bi-eye-slash');
+    }
+    else{
+        input.setAttribute('type', 'password');
+        icone.classList.replace('bi-eye-slash', 'bi-eye');
+    }
+}
