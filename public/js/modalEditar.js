7770eaa0a455a33fa2845950ea89f57ebfb115b9
Guilherme Tavares <tavaresguilherme723@gmail.com>
2025-05-14 17:14:31 -0300

    cria botão fechar modal edit posts

--- public/js/modalEditar.js
+++ public/js/modalEditar.js
@@ -3,3 +3,8 @@ const inputImg = document.getElementById('inputImgEditar');
 inputImg.onchange = function(){
     image.src = URL.createObjectURL(inputImg.files[0]);
 }
+
+function modalEditar(){
+    let modalEditar = document.getElementById('modalEdit');
+    modalEditar.style.display = 'block';    
+}
