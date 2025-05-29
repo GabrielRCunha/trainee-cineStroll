640bc451be57dd948bc60d99b096467732f38f3e
Guiba1 <tavares.guilherme@estudante.ufjf.br>
2025-05-29 17:26:17 -0300

    adiciona rota

---
+++ app/Controllers/postsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+use Exception;
+
+class postsController
+{
+
+    public function index()
+    {
+        return view('admin/index');
+    }
+}
